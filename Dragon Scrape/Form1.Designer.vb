144bb7e8b07421a286fffcd223e49a24a32835c6
Justin L. Ross | Alias: David | Lucian Patterson <88350777+Rythorian77@users.noreply.github.com>
2022-05-17 19:52:04 -0400

    Add files via upload

---
+++ Dragon Scrape/Form1.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.UrlHost = New System.Windows.Forms.TextBox()
+        Me.Seeker = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Png = New System.Windows.Forms.CheckBox()
+        Me.Tif = New System.Windows.Forms.CheckBox()
+        Me.Gif = New System.Windows.Forms.CheckBox()
+        Me.Jpg = New System.Windows.Forms.CheckBox()
+        Me.Bmp = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Clear = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'UrlHost
+        '
+        Me.UrlHost.BackColor = System.Drawing.SystemColors.InactiveCaptionText
+        Me.UrlHost.Font = New System.Drawing.Font("MS Reference Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.UrlHost.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.UrlHost.Location = New System.Drawing.Point(207, 174)
+        Me.UrlHost.Multiline = True
+        Me.UrlHost.Name = "UrlHost"
+        Me.UrlHost.Size = New System.Drawing.Size(672, 36)
+        Me.UrlHost.TabIndex = 0
+        '
+        'Seeker
+        '
+        Me.Seeker.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Seeker.Font = New System.Drawing.Font("MS Reference Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Seeker.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.Seeker.Location = New System.Drawing.Point(910, 166)
+        Me.Seeker.Name = "Seeker"
+        Me.Seeker.Size = New System.Drawing.Size(181, 48)
+        Me.Seeker.TabIndex = 1
+        Me.Seeker.Text = "Get Em'"
+        Me.Seeker.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.Label1.Location = New System.Drawing.Point(169, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(783, 35)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Please Insert The Web Address To Acquire Images"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(-2, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(144, 139)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(959, 0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(144, 139)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'Png
+        '
+        Me.Png.AutoSize = True
+        Me.Png.Font = New System.Drawing.Font("MS Reference Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Png.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Png.Location = New System.Drawing.Point(322, 109)
+        Me.Png.Name = "Png"
+        Me.Png.Size = New System.Drawing.Size(84, 30)
+        Me.Png.TabIndex = 5
+        Me.Png.Text = "PNG"
+        Me.Png.UseVisualStyleBackColor = True
+        '
+        'Tif
+        '
+        Me.Tif.AutoSize = True
+        Me.Tif.Font = New System.Drawing.Font("MS Reference Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Tif.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Tif.Location = New System.Drawing.Point(412, 109)
+        Me.Tif.Name = "Tif"
+        Me.Tif.Size = New System.Drawing.Size(73, 30)
+        Me.Tif.TabIndex = 6
+        Me.Tif.Text = "TIF"
+        Me.Tif.UseVisualStyleBackColor = True
+        '
+        'Gif
+        '
+        Me.Gif.AutoSize = True
+        Me.Gif.Font = New System.Drawing.Font("MS Reference Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Gif.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Gif.Location = New System.Drawing.Point(491, 109)
+        Me.Gif.Name = "Gif"
+        Me.Gif.Size = New System.Drawing.Size(77, 30)
+        Me.Gif.TabIndex = 7
+        Me.Gif.Text = "GIF"
+        Me.Gif.UseVisualStyleBackColor = True
+        '
+        'Jpg
+        '
+        Me.Jpg.AutoSize = True
+        Me.Jpg.Font = New System.Drawing.Font("MS Reference Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Jpg.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Jpg.Location = New System.Drawing.Point(574, 109)
+        Me.Jpg.Name = "Jpg"
+        Me.Jpg.Size = New System.Drawing.Size(78, 30)
+        Me.Jpg.TabIndex = 8
+        Me.Jpg.Text = "JPG"
+        Me.Jpg.UseVisualStyleBackColor = True
+        '
+        'Bmp
+        '
+        Me.Bmp.AutoSize = True
+        Me.Bmp.Font = New System.Drawing.Font("MS Reference Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Bmp.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Bmp.Location = New System.Drawing.Point(658, 109)
+        Me.Bmp.Name = "Bmp"
+        Me.Bmp.Size = New System.Drawing.Size(84, 30)
+        Me.Bmp.TabIndex = 9
+        Me.Bmp.Text = "BMP"
+        Me.Bmp.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MS Reference Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.Label2.Location = New System.Drawing.Point(308, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(443, 28)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Select as many formats as you wish"
+        '
+        'Clear
+        '
+        Me.Clear.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Clear.Font = New System.Drawing.Font("MS Reference Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Clear.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.Clear.Location = New System.Drawing.Point(12, 168)
+        Me.Clear.Name = "Clear"
+        Me.Clear.Size = New System.Drawing.Size(181, 48)
+        Me.Clear.TabIndex = 11
+        Me.Clear.Text = "Clear Settings"
+        Me.Clear.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText
+        Me.TextBox1.Font = New System.Drawing.Font("MS Reference Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.TextBox1.Location = New System.Drawing.Point(12, 335)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(1079, 333)
+        Me.TextBox1.TabIndex = 12
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("MS Reference Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.Label3.Location = New System.Drawing.Point(279, 290)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(538, 28)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "HTML Code Display From Your Web Address"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("MS Reference Sans Serif", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.Label4.Location = New System.Drawing.Point(48, 237)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(1007, 28)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Images and an HTML file will be saved to the desktop in a folder called: Collecti" &
+    "on"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(1103, 680)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Clear)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Bmp)
+        Me.Controls.Add(Me.Jpg)
+        Me.Controls.Add(Me.Gif)
+        Me.Controls.Add(Me.Tif)
+        Me.Controls.Add(Me.Png)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Seeker)
+        Me.Controls.Add(Me.UrlHost)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Dragon Image Scraper"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents UrlHost As TextBox
+    Friend WithEvents Seeker As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Png As CheckBox
+    Friend WithEvents Tif As CheckBox
+    Friend WithEvents Gif As CheckBox
+    Friend WithEvents Jpg As CheckBox
+    Friend WithEvents Bmp As CheckBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Clear As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Dragon Scrape/Form1.vb
@@ -0,0 +1,169 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Private ReadOnly htmlsource As String = My.Computer.FileSystem.SpecialDirectories.Desktop & "\Collection\source.txt"
+    Private ReadOnly portfolio As String = My.Computer.FileSystem.SpecialDirectories.Desktop & "\Collection\images"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+        'This will create a desktop folder if one doesn't exist
+        If Not Directory.Exists($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\Collection") Then
+            Directory.CreateDirectory($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\Collection")
+        End If
+
+        PictureBox1.Load(AppDomain.CurrentDomain.BaseDirectory & "../../Images/face.jpg")
+        PictureBox2.Load(AppDomain.CurrentDomain.BaseDirectory & "../../Images/face.jpg")
+    End Sub
+
+
+    Function GetImagesFromURL(URL As String)
+        Dim i As Integer = -1
+        Dim Picturez(3000) As Image
+        Dim cl As HttpWebRequest
+        cl = WebRequest.Create(URL)
+        Dim receive As HttpWebResponse = cl.GetResponse
+        Dim WStream As Stream = receive.GetResponseStream
+
+        ' Create reader object
+        Dim Basin As New IO.StreamReader(WStream)
+        Dim items As String = Basin.ReadToEnd
+        'Why use this expression??? >>> "src\=\""[\u0000-\uFFFF]*?\.(gif|png|jpg|tif|bmp)\""" 
+        'It searches for a string like src="AnyCharForMinimumLength.gif or png or jpg or tif or bmp"
+        For Each Rythorian As Match In Regex.Matches(items, "src\=\""[\u0000-\uFFFF]*?\.(gif|png|jpg|tif|bmp)\""") 'for each match"
+
+            Dim Kreznok As String = Rythorian.Value
+            Kreznok = Kreznok.TrimStart("s", "r", "c", "=", """").TrimEnd("""") ' it clears the url from src=" and "
+
+            Try
+                Try
+                    'if is a full url then create a new web request
+                    cl = WebRequest.Create(Kreznok) 'if this fails then this is not a complete url
+                Catch ex As Exception
+                    ' if is not the full url then combine the given URL with the image path and get rid of any \\ if they exists ( www.mysite.com//image.gif to www.mysite.com/image.gif)
+                    cl = WebRequest.Create((URL & "/" & Kreznok).Replace("//", "/"))
+
+                End Try
+
+                receive = cl.GetResponse
+                WStream = receive.GetResponseStream
+
+                i += 1
+                'write the image into an array(list) of images
+                Picturez(i) = Image.FromStream(WStream)
+            Catch ex As Exception
+
+            End Try
+
+        Next
+
+        Return Picturez
+
+    End Function
+
+    Private Sub Seeker_Click(sender As Object,
+                             e As EventArgs) Handles Seeker.Click
+
+        Dim i As Integer = 0
+        For Each img As Image In GetImagesFromURL(UrlHost.Text.ToString) '"https://moviestowatch.tv/movie"
+            Try
+                If img IsNot Nothing Then
+
+                    If Png.Checked Then
+                        img.Save(filename:=$"{portfolio}{i}.png")
+                        i += 1
+                    ElseIf Gif.Checked Then
+                        img.Save(filename:=$"{portfolio}{i}.gif")
+                        i += 1
+                    ElseIf Jpg.Checked Then
+                        img.Save(filename:=$"{portfolio}{i}.jpg")
+                        i += 1
+                    ElseIf Bmp.Checked Then
+                        img.Save(filename:=$"{portfolio}{i}.bmp")
+                        i += 1
+                    ElseIf Tif.Checked Then
+                        img.Save(filename:=$"{portfolio}{i}.tif")
+                        i += 1
+                    End If
+
+                End If
+            Catch ex As Exception
+                'when it's done it gives an error telling us that there is no image in the next img, so we stop
+                Exit For
+            End Try
+
+        Next
+
+
+        Dim request As WebRequest = WebRequest.Create(UrlHost.Text)
+
+        Using response As WebResponse = request.GetResponse()
+            Using reader As New StreamReader(response.GetResponseStream())
+                Dim html As String = reader.ReadToEnd()
+                File.WriteAllText(htmlsource, html)
+            End Using
+        End Using
+
+        Try
+            Dim array = File.ReadAllLines(htmlsource)
+            For i1 = 0 To array.Length - 1
+                Dim sz As String = array(i1)
+                TextBox1.AppendText(sz + vbNewLine)
+
+            Next
+
+        Catch ex As Exception
+
+            MessageBox.Show(ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object,
+                                  e As FormClosingEventArgs) Handles Me.FormClosing
+
+        Dispose()
+        GC.Collect()
+        GC.WaitForPendingFinalizers()
+    End Sub
+
+    Private Sub Png_CheckedChanged(sender As Object,
+                                   e As EventArgs) Handles Png.CheckedChanged
+
+    End Sub
+
+    Private Sub Tif_CheckedChanged(sender As Object,
+                                   e As EventArgs) Handles Tif.CheckedChanged
+
+    End Sub
+
+    Private Sub Gif_CheckedChanged(sender As Object,
+                                   e As EventArgs) Handles Gif.CheckedChanged
+
+    End Sub
+
+    Private Sub Jpg_CheckedChanged(sender As Object,
+                                   e As EventArgs) Handles Jpg.CheckedChanged
+
+    End Sub
+
+    Private Sub Bmp_CheckedChanged(sender As Object,
+                                   e As EventArgs) Handles Bmp.CheckedChanged
+
+    End Sub
+
+    Private Sub Clear_Click(sender As Object,
+                            e As EventArgs) Handles Clear.Click
+
+        Png.CheckState = CheckState.Unchecked
+        Tif.CheckState = CheckState.Unchecked
+        Gif.CheckState = CheckState.Unchecked
+        Bmp.CheckState = CheckState.Unchecked
+        Jpg.CheckState = CheckState.Unchecked
+
+    End Sub
+End Class
